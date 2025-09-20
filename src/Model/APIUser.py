5929534c6817d2e0fa1b07e69e46ca897b156b00
LouisSta <louis.stanisiere@eleve.ensai.fr>
2025-09-21 01:14:48 +0200

    restructure_init

--- src/Model/APIUser.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class APIUser(BaseModel):
-    id: int
-    username: str
--- src/Model/JWTResponse.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-
-class JWTResponse(BaseModel):
-    access_token: str
