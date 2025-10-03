1659879e8030bef5d7a600e8617cb15a7d071af9
LouisSta <louis.stanisiere@eleve.ensai.fr>
2025-10-03 17:00:39 +0200

    fini

---
+++ src/service/response_service.py
@@ -0,0 +1,4 @@
+class ResponseService:
+    def __init__(self, code: int, content: str):
+        self.code = code
+        self.content = content
--- tests/service/test_user_service.py
+++ tests/service/test_user_service.py
@@ -9,7 +9,7 @@ def user_service():
 
 
 def test_get_user_not_found(user_service):
-    # GIVEN : rien
+    # GIVEN : aucun utilisateur créé
 
     # WHEN
     result = user_service.get_user(1)
@@ -19,7 +19,7 @@ def test_get_user_not_found(user_service):
 
 
 def test_get_user_by_username_not_found(user_service):
-    # GIVEN : rien
+    # GIVEN : aucun utilisateur créé
 
     # WHEN
     result = user_service.get_user_by_username("Inconnu")
@@ -37,7 +37,8 @@ def test_create_user_success(user_service):
     result = user_service.create_user(username, password)
 
     # THEN
-    assert result is True
+    assert result.code == 201
+    assert result.content == "Utilisateur créé avec succès"
     assert user_service.get_user_by_username("Louis") is not None
 
 
@@ -45,25 +46,25 @@ def test_create_user_duplicate(user_service):
     # GIVEN
     user_service.create_user("Louis", "UnMotDePasseSecure*159")
 
-    # WHEN / THEN
-    with pytest.raises(ValueError, match="Ce nom d'utilisateur est déjà pris"):
-        user_service.create_user("Louis", "UnAutreMotDePasseSecure*159")
+    # WHEN
+    result = user_service.create_user("Louis", "UnAutreMotDePasseSecure*159")
 
     # THEN
-    user = user_service.get_user_by_username("Louis")
-    assert user is not None
+    assert result.code == 409
+    assert result.content == "Nom d'utilisateur déjà utilisé"
     assert user_service.count_users() == 1
 
 
 def test_create_user_not_secure_password(user_service):
-    # GIVEN : nothing
+    # GIVEN : aucun utilisateur créé
 
-    #WHEN/THEN
-    with pytest.raises(ValueError, match="Le mot de passe n'est pas assez sécurisé."):
-        user_service.create_user("Louis", "mdp")
+    # WHEN
+    result = user_service.create_user("Louis", "mdp")
+
     # THEN
-    user = user_service.get_user_by_username("Louis")
-    assert user is None
+    assert result.code == 400
+    assert result.content == "Mot de passe trop faible"
+    assert user_service.get_user_by_username("Louis") is None
     assert user_service.count_users() == 0
 
 
@@ -75,7 +76,8 @@ def test_create_two_user_success(user_service):
     result = user_service.create_user("louis", "UnMotDePasseSecure*159")
 
     # THEN
-    assert result is True
+    assert result.code == 201
+    assert result.content == "Utilisateur créé avec succès"
     first_user = user_service.get_user_by_username("Louis")
     second_user = user_service.get_user_by_username("louis")
     assert first_user.username == "Louis"
@@ -90,7 +92,8 @@ def test_authenticate_success(user_service):
     result = user_service.authenticate("Louis", "UnMotDePasseSecure*159")
 
     # THEN
-    assert result is True
+    assert result.code == 200
+    assert result.content == "Authentification réussie"
 
 
 def test_authenticate_failure(user_service):
@@ -98,83 +101,85 @@ def test_authenticate_failure(user_service):
     user_service.create_user("Louis", "UnMotDePasseSecure*159")
 
     # WHEN
-    result1 = user_service.authenticate("louis", "UnMotDePasseSecure*159")  #lowercase
+    result1 = user_service.authenticate("louis", "UnMotDePasseSecure*159")
     result2 = user_service.authenticate("Louis", "mauvais_mdp")
 
     # THEN
-    assert result1 is False
-    assert result2 is False
+    assert result1.code == 401
+    assert result2.code == 401
+    assert result1.content == "Nom d'utilisateur ou mot de passe incorrect"
+    assert result2.content == "Nom d'utilisateur ou mot de passe incorrect"
 
 
 def test_change_password_success(user_service):
-    #GIVEN
+    # GIVEN
     user_service.create_user("Louis", "UnMotDePasseSecure*159")
 
-    #WHEN
+    # WHEN
     result = user_service.change_password("Louis", "UnMotDePasseSecure*159",
                                           "AutreMotDePasseSecure*159")
-    result_auth = user_service.authenticate("Louis",  "AutreMotDePasseSecure*159")
+    result_auth = user_service.authenticate("Louis", "AutreMotDePasseSecure*159")
     result_wrong_auth = user_service.authenticate("Louis", "UnMotDePasseSecure*159")
 
-    #THEN
-    assert result is True
-    assert result_auth is True
-    assert result_wrong_auth is False
+    # THEN
+    assert result.code == 200
+    assert result.content == "Mot de passe modifié avec succès"
+    assert result_auth.code == 200
+    assert result_wrong_auth.code == 401
 
 
 def test_change_password_failure(user_service):
-    #GIVEN
+    # GIVEN
     user_service.create_user("Louis", "UnMotDePasseSecure*159")
 
-    #WHEN/THEN
-    with pytest.raises(ValueError, match="Mot de passe incorrect."):
-        user_service.change_password("Louis", "mauvais_mdp",
-                                     "159*AutreMotDePasseSecure")
-    #WHEN/THEN
-    with pytest.raises(ValueError, match="Le mot de passe n'est pas assez sécurisé."):
-        user_service.change_password("Louis", "UnMotDePasseSecure*159", "mdp")
-
-    #WHEN
-    result = user_service.change_password("Alice", "UnMotDePasseSecure*159",
-                                          "159*AutreMotDePasseSecure")
+    # WHEN
+    result_wrong_password = user_service.change_password("Louis", "mauvais_mdp",
+                                                         "159*AutreMotDePasseSecure")
+    result_weak_password = user_service.change_password("Louis",
+                                                        "UnMotDePasseSecure*159", "mdp")
+    result_user_not_found = user_service.change_password("Alice",
+                                                         "UnMotDePasseSecure*159",
+                                                         "159*AutreMotDePasseSecure")
     result_auth = user_service.authenticate("Louis", "UnMotDePasseSecure*159")
     result_wrong_auth = user_service.authenticate("Louis", "159*AutreMotDePasseSecure")
 
-    #THEN
-    assert result is False
-    assert result_auth is True
-    assert result_wrong_auth is False
+    # THEN
+    assert result_wrong_password.code == 401
+    assert result_weak_password.code == 400
+    assert result_user_not_found.code == 404
+    assert result_auth.code == 200
+    assert result_wrong_auth.code == 401
 
 
 def test_change_username_success(user_service):
-    #GIVEN
+    # GIVEN
     user_service.create_user("Louis", "UnMotDePasseSecure*159")
 
-    #WHEN
+    # WHEN
     result = user_service.change_username("Louis", "Bob")
-    result_auth = user_service.authenticate("Bob",  "UnMotDePasseSecure*159")
+    result_auth = user_service.authenticate("Bob", "UnMotDePasseSecure*159")
     result_wrong_auth = user_service.authenticate("Louis", "UnMotDePasseSecure*159")
 
-    #THEN
-    assert result is True
-    assert result_auth is True
-    assert result_wrong_auth is False
+    # THEN
+    assert result.code == 200
+    assert result.content == "Nom d'utilisateur modifié avec succès"
+    assert result_auth.code == 200
+    assert result_wrong_auth.code == 401
 
 
 def test_change_username_failure(user_service):
-    #GIVEN
+    # GIVEN
     user_service.create_user("Louis", "UnMotDePasseSecure*159")
     user_service.create_user("Alice", "A159*MotDePasseSecure")
 
-    #WHEN
-
-    with pytest.raises(ValueError, match="Le nom d'utilisateur Alice est déjà pris."):
-        user_service.change_username("Louis", "Alice")
-    result2 = user_service.change_username("Bob", "Bob2")
-    result_auth = user_service.authenticate("Louis",  "UnMotDePasseSecure*159")
+    # WHEN
+    result_duplicate = user_service.change_username("Louis", "Alice")
+    result_user_not_found = user_service.change_username("Bob", "Bob2")
+    result_auth = user_service.authenticate("Louis", "UnMotDePasseSecure*159")
     result_wrong_auth = user_service.authenticate("Bob2", "159*AutreMotDePasseSecure")
 
-    #THEN
-    assert result_auth is True
-    assert result_wrong_auth is False
-    assert result2 is False
+    # THEN
+    assert result_duplicate.code == 409
+    assert result_user_not_found.code == 404
+    assert result_auth.code == 200
+    assert result_wrong_auth.code == 401
