502352f5e71fb2496f1108e2249ec8e6e1513eef
id2933 <kossi-bruno.amah@eleve.ensai.fr>
2025-10-03 15:05:31 +0000

    mise à jour des vues

---
+++ src/view_inq/accueil/connexion_view.py
@@ -0,0 +1,10 @@
+from abstract_view import VueAbstraite
+# from InquirerPy import inquirer
+
+
+class VueConnexion(VueAbstraite):
+    def __init__(self, message):
+        super().__init__(message)
+
+    def choisir_menu(self):
+        pass
---
+++ src/view_inq/accueil/vue_accueil.py
@@ -0,0 +1,33 @@
+from abstract_view import VueAbstraite
+from InquirerPy import inquirer
+
+
+class VueAcceuil(VueAbstraite):
+    def __init__(self, message):
+        super().__init__(message)
+
+    def choisir_menu(self):
+
+        """Choix du menu suivant
+
+        Return
+        ------
+        view
+            Retourne la vue choisie par l'utilisateur dans le terminal
+        """
+
+        print("\n" + "-" * 50 + "\nAccueil\n" + "-" * 50 + "\n")
+        choix = inquirer.select(
+            message="Que voulez-vous faire?",
+            choices=["Se connecter", "Creer un compte", "Quitter"],
+        ).execute()
+
+        match choix:
+            case "Se connecter":
+                from view_inq.accueil.connexion_view import VueConnexion
+                return VueConnexion("Connexion à l'application")
+            case "Creer un compte":
+                from view_inq.accueil.inscription_view import VueInscription
+                return VueInscription("Création de compte joueur")
+            case "Quitter":
+                print("Vous avez cliqué sur: [Quitter]")
