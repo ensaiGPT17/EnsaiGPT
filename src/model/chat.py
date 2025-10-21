1e9a0447df8af8f00f173a40510c5f5f6f2bd6b2
id2933 <kossi-bruno.amah@eleve.ensai.fr>
2025-10-21 09:47:10 +0000

    un detail

--- src/model/chat.py
+++ src/model/chat.py
@@ -10,6 +10,20 @@ class Chat:
         self.title = title
         self.start_date = start_date
         self.last_date = last_date
+
+        # Yassine, c'est Bruno, (je n'ai pas fait le travail à ta place
+        # ce fichier existe depuis plus d'une semaine avant la repartition des taches)
+
+        # TU PEUX ME POSER DES QUESTIONS SI CE N'EST PAS CLAIR
+        
+        # ICi on garde une trace de la liste de messages echangé entre
+        # l'assistant et le user
+        self.history: List[Dict[str, str]] = []
+
+        # il s'agit d'une liste de dic, chaque dictionnaire 
+        # est ceomme ceci: {'role': "assisant/user", 
+        #                           "content": "contenu du message recu/envoyé"}
+
         self.max_tokens = max_tokens
         self.top_p = top_p
         self.temperature = temperature
