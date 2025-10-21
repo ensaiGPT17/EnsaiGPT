367fbcfdbcdb04b8a52da542a69494b2b9984fe4
id2743 <elsa.laviec@eleve.ensai.fr>
2025-10-21 10:18:05 +0000

    squelette messageDAO

--- src/dao/message_dao.py
+++ src/dao/message_dao.py
@@ -9,8 +9,50 @@ class MessageDAO(metaclass=Singleton):
     def __init__(self):
         pass
 
-    def créer_message(self, id_chat: int, role: str, content: str) -> Optional[Message]:
+    def create_message(self, id_chat: int, role: str, content: str) -> Optional[Message]:
+        """ ajouter un nouveau message a la bdd
+        
+        Parameters
+        ----------
+        id_chat : l'id de la conversation a laquelle le message appartient
+        role : l'auteur du message 
+        content : le contenu du message 
+        
+        Returns 
+        --------
+        L'objet Message cree 
+        ou None en cas d'errreur 
+
+        """
         pass
 
-    def supprimer_message(self, id_message: int) -> bool:
+    def delete_message(self, id_message: int) -> bool:
+        """supprimer un message de la bdd
+        
+        Parameters
+        ----------
+        id_message : l'id du message a supprimer
+        
+        Returns 
+        -------
+        bool : true si le message a bien ete supprime
+        
+        """
         pass
+
+    def get_message_by_id(self, id_message: int) -> Optional[Message]:
+        """ recuperer un message par son id
+        
+        Parameters 
+        -----------
+        id_message : l'identifiant du message 
+
+        Returns 
+        --------
+        Un objet Message s'il existe
+        None sinon
+        
+        """
+        pass
+
+    # def get_messages_by_chat(self, id_chat) ici ou dans chatDAO ?
