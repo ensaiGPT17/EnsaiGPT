453d451e2c0e0cbc465d168368ff06643489cf45
id2743 <elsa.laviec@eleve.ensai.fr>
2025-11-05 14:32:16 +0000

    vues

--- src/view/userviews/search_conversation_view.py
+++ src/view/userviews/search_conversation_view.py
@@ -1,6 +1,10 @@
 from view.abstract_view import AbstractView
 from InquirerPy import inquirer
 from view.session import Session
+from service.chat_service import ChatService
+from dao.chat_dao import ChatDAO
+from datetime import datetime
+from model.chat import Chat
 
 class SearchConversationView(AbstractView):
     def __init__(self, message: str = ""):
@@ -9,22 +13,66 @@ class SearchConversationView(AbstractView):
     def choisir_menu(self):
         user = Session().user
         username = user.username
-        print("\n" + "-" * 50 + "\nDémarrer une conversation\n" + "-" * 50 + "\n")
+        chat_dao = ChatDAO()
+        chat_service = ChatService(chat_dao)
+
+        print("\n" + "-" * 50 + "\nRechercher une conversation\n" + "-" * 50 + "\n")
         choix = inquirer.select(
             message=f"Que voulez-vous faire {username} ?",
             choices=[
-                "Continuer",
-                "Configurer les paramètres",
+                "Rechercher une conversation par mot-clé dans le titre",
+                "Rechercher une conversation par date",
                 "Retour"               
             ],
         ).execute()
 
-        if choix == "Continuer":
-            from view.userviews.discussion_view import DiscussionView
-            return DiscussionView("Discussion")
-        elif choix == "Configurer les paramètres":
-            from view.userviews.change_params_view import ChangeConvParamsView
-            return ChangeConvParamsView("Configurer les paramètres")
+        if choix == "Rechercher une conversation par mot-clé dans le titre":
+            # demande du mot clé à l'utilisateur
+            mot_cle = inquirer.text("Rentrez un mot-clé pour la recherche: ").execute()
+            # appel a la fonction dans chat service : 
+            #res = chat_service.search_chat_by_title(mot_cle)
+            max_tokens = 4000
+            top_p = 0.5
+            temperature = 0.5
+
+            res = [
+            Chat(50, 2, "Conversation sur l'IA et les technologies", 
+                datetime.now(), datetime.now(), max_tokens, top_p, temperature),
+            Chat(50, 2, "Projet Machine Learning", 
+                datetime.now(), datetime.now(), max_tokens, top_p, temperature),
+            Chat(50, 2, "Discussion sur les statistiques", 
+                datetime.now(), datetime.now(), max_tokens, top_p, temperature),
+            Chat(50, 2, "Analyse de données et Visualisation", 
+                datetime.now(), datetime.now(), max_tokens, top_p, temperature)
+            ]   
+            # si pas d'erreur : la fonction doit renvoyer liste des conversations correspondantes
+            # on bascule dans la vue voir historique 
+
+            from view.userviews.see_historic_view import SeeHistoricView
+            return SeeHistoricView(message="Voir l'historique de conversation", conv_list=res)
+            
+            # si erreur : a faire 
+
+
+            print("fonctionnalité non implémentée")
+            return self
+
+        elif choix == "Rechercher une conversation par date":
+            # demande de la date à l'utilisateur et convertir en datetime 
+            date = inquirer.text("Rentrez une date pour la recherche (YYYY-MM-DD): ").execute()
+            date_convertie = datetime.strptime(date, "%Y-%m-%d")
+            # appel a la fonction dans chat service 
+            res = chat_service.search_chat_by_date(date_convertie)
+
+            # si pas d'erreur : la fonction doit renvoyer liste des conv 
+    
+            
+
+
+            # si erreur : 
+
+            print("fonctionnalité non implémentée")
+            return self
         elif choix == "Retour":
-            from view.userviews.main_menu_view import MainMenuView
-            return MainMenuView("Menu Principal")
+            from view.userviews.historic_conversation_view import HistoricConversationView
+            return HistoricConversationView("Historique de conversation")
--- src/view/userviews/see_historic_view.py
+++ src/view/userviews/see_historic_view.py
@@ -7,7 +7,7 @@ from model.chat import Chat
 from datetime import datetime
 
 class SeeHistoricView(AbstractView):
-    def __init__(self, message: str = "", conv_list = conv_list):
+    def __init__(self, message: str = "", conv_list=[]):
         super().__init__(message)
         self.conv_list = conv_list
 
@@ -22,22 +22,13 @@ class SeeHistoricView(AbstractView):
         top_p = 0.5
         temperature = 0.5
 
-        conv_list = [
-            Chat(50, 2, "Conversation sur l'IA et les technologies", 
-                datetime.now(), datetime.now(), max_tokens, top_p, temperature),
-            Chat(50, 2, "Projet Machine Learning", 
-                datetime.now(), datetime.now(), max_tokens, top_p, temperature),
-            Chat(50, 2, "Discussion sur les statistiques", 
-                datetime.now(), datetime.now(), max_tokens, top_p, temperature),
-            Chat(50, 2, "Analyse de données et Visualisation", 
-                datetime.now(), datetime.now(), max_tokens, top_p, temperature)
-        ]
+        
         # Largeur totale pour la ligne (par exemple, 40 caractères)
         total_width = 60
 
         # Formatage de la liste des conversations avec un compteur i et des underscores ajustés
         formated_conv_list = []
-        for i, conv in enumerate(conv_list, start=1):
+        for i, conv in enumerate(self.conv_list, start=1):
             # Formatage de la date en accédant correctement à l'objet datetime dans le tuple
             formatted_date = conv.last_date[0].strftime("%Y-%m-%d %H:%M:%S")
 
