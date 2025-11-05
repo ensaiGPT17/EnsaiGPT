fd2d527ba41643cb24621515d7f0cb34817ae2b2
id2743 <elsa.laviec@eleve.ensai.fr>
2025-11-05 15:46:08 +0000

    chat dao ajout deux methodes

--- src/view/userviews/search_conversation_view.py
+++ src/view/userviews/search_conversation_view.py
@@ -30,7 +30,9 @@ class SearchConversationView(AbstractView):
             # demande du mot clé à l'utilisateur
             mot_cle = inquirer.text("Rentrez un mot-clé pour la recherche: ").execute()
             # appel a la fonction dans chat service : 
-            #res = chat_service.search_chat_by_title(mot_cle)
+            # res = chat_service.search_chat_by_title(mot_cle)
+
+            # liste provisoire pour tester
             max_tokens = 4000
             top_p = 0.5
             temperature = 0.5
@@ -47,13 +49,10 @@ class SearchConversationView(AbstractView):
             ]   
             # si pas d'erreur : la fonction doit renvoyer liste des conversations correspondantes
             # on bascule dans la vue voir historique 
-
             from view.userviews.list_conversation_view import ListConversationView
             return ListConversationView(message="Voir l'historique de conversation", conv_list=res, last_view=1)
             
             # si erreur : a faire 
-
-
             print("fonctionnalité non implémentée")
             return self
 
@@ -62,14 +61,28 @@ class SearchConversationView(AbstractView):
             date = inquirer.text("Rentrez une date pour la recherche (YYYY-MM-DD): ").execute()
             date_convertie = datetime.strptime(date, "%Y-%m-%d")
             # appel a la fonction dans chat service 
-            res = chat_service.search_chat_by_date(date_convertie)
+            # res = chat_service.search_chat_by_date(date_convertie)
 
-            # si pas d'erreur : la fonction doit renvoyer liste des conv 
-    
+            # liste provisoire pour tester 
+            max_tokens = 4000
+            top_p = 0.5
+            temperature = 0.5
+
+            res = [
+            Chat(50, 2, "Conversation sur l'IA et les technologies", 
+                datetime.now(), datetime.now(), max_tokens, top_p, temperature),
+            Chat(50, 2, "Projet Machine Learning", 
+                datetime.now(), datetime.now(), max_tokens, top_p, temperature),
+            Chat(50, 2, "Discussion sur les statistiques", 
+                datetime.now(), datetime.now(), max_tokens, top_p, temperature),
+            Chat(50, 2, "Analyse de données et Visualisation", 
+                datetime.now(), datetime.now(), max_tokens, top_p, temperature)
+            ]   
+             
+            from view.userviews.list_conversation_view import ListConversationView
+            return ListConversationView(message="Voir l'historique de conversation", conv_list=res, last_view=1)
             
-
-
-            # si erreur : 
+            # si erreur : a faire 
 
             print("fonctionnalité non implémentée")
             return self
