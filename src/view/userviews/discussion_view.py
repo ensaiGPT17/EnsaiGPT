2c63d15c0c85f2c0422c1ad4d7032bbfb8b42c1a
id2933 <kossi-bruno.amah@eleve.ensai.fr>
2025-11-06 14:07:30 +0000

    premier message

--- src/view/userviews/discussion_view.py
+++ src/view/userviews/discussion_view.py
@@ -10,11 +10,12 @@ class DiscussionView(AbstractView):
         self.conversation = [
             (message.role_author, message.content) for message in liste_message
         ]  # Liste de tuples (role, message)
-        self.title = chat.title
+        self.chat = chat
+        self.liste_messages = liste_message
 
     def afficher_conversation(self):
         print("\n" + "-"*50)
-        print("Discussion".center(50))
+        print(f"Titre: {self.chat.title}".center(50))
         print("-"*50 + "\n")
 
         # Affichage de la conversation
@@ -42,12 +43,38 @@ class DiscussionView(AbstractView):
             ).execute()
 
             if choix == "Envoyer un message":
+                """
                 message_user = inquirer.text(message="Votre message :").execute()
                 self.conversation.append(("user", message_user))
 
                 # Simulation de réponse de l’assistant (placeholder)
                 message_assistant = f"Réponse à : '{message_user}'"  # À remplacer par un vrai modèle si besoin
                 self.conversation.append(("assistant", message_assistant))
+                """
+                
+                from service.chat_service import ChatService
+                from dao.chat_dao import ChatDAO
+                from service.message_service import MessageService
+                from dao.message_dao import MessageDAO
+                from view.userviews.discussion_view import DiscussionView
+
+                message_user = inquirer.text(message="Votre message :").execute()
+                self.conversation.append(("user", message_user))
+
+                chat_service = ChatService(ChatDAO())
+                message_service = MessageService(MessageDAO())
+
+                #new_chat = chat_service.create_chat(message_user, user.id_user)
+
+                message_list = chat_service.send_message(
+                   chat=self.chat,
+                    history=self.liste_messages,
+                    content=message_user
+                )
+                
+                message_list = message_list[1:]
+                return DiscussionView(chat=self.chat, liste_message=message_list, first_time=1)
+
 
             elif choix == "Quitter la discussion":
                 from view.userviews.main_menu_view import MainMenuView
