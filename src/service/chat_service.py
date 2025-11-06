3c036999815a24bcab30584f3ba2cb5a517e5169
LouisSta <louis.stanisiere@eleve.ensai.fr>
2025-11-06 11:06:41 +0100

    Update chat_service.py

--- src/service/chat_service.py
+++ src/service/chat_service.py
@@ -51,9 +51,7 @@ class ChatService:
     @log
     def request_title(self, id_chat: int) -> str:
         history = self.message_service.get_messages_by_chat(id_chat)
-        history.append(self.message_service.title_request()) # methode a implementer
-        # cette methode devra renvoyer un objet Message avec le contenu de la demande
-        # de titre
+        history.append(self.message_service.title_request())
         chat = self.get_chat(id_chat)
         return self.client.generate(chat, history)
 
