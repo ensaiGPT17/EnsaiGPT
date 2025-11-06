ac78073ee25c3d27354a17e66a71b3ba3321440c
id2743 <elsa.laviec@eleve.ensai.fr>
2025-11-06 09:25:09 +0000

    vue recherche

--- src/view/userviews/search_conversation_view.py
+++ src/view/userviews/search_conversation_view.py
@@ -30,9 +30,9 @@ class SearchConversationView(AbstractView):
             # demande du mot clé à l'utilisateur
             mot_cle = inquirer.text("Rentrez un mot-clé pour la recherche: ").execute()
             # appel a la fonction dans chat service : 
-            # res = chat_service.search_chat_by_title(mot_cle)
+            res = chat_service.search_chat_by_title(mot_cle)
 
-            # liste provisoire pour tester
+            """ liste provisoire pour tester
             max_tokens = 4000
             top_p = 0.5
             temperature = 0.5
@@ -46,7 +46,7 @@ class SearchConversationView(AbstractView):
                 datetime.now(), datetime.now(), max_tokens, top_p, temperature),
             Chat(50, 2, "Analyse de données et Visualisation", 
                 datetime.now(), datetime.now(), max_tokens, top_p, temperature)
-            ]   
+            ]   """
             # si pas d'erreur : la fonction doit renvoyer liste des conversations correspondantes
             # on bascule dans la vue voir historique 
             from view.userviews.list_conversation_view import ListConversationView
@@ -57,13 +57,13 @@ class SearchConversationView(AbstractView):
             return self
 
         elif choix == "Rechercher une conversation par date":
-            # demande de la date à l'utilisateur et convertir en datetime 
+            # demande de la date à l'utilisateur
             date = inquirer.text("Rentrez une date pour la recherche (YYYY-MM-DD): ").execute()
-            date_convertie = datetime.strptime(date, "%Y-%m-%d")
+            #date_convertie = datetime.strptime(date, "%Y-%m-%d")
             # appel a la fonction dans chat service 
-            # res = chat_service.search_chat_by_date(date_convertie)
+            res = chat_service.search_chat_by_date(date_convertie)
 
-            # liste provisoire pour tester 
+            """ liste provisoire pour tester 
             max_tokens = 4000
             top_p = 0.5
             temperature = 0.5
@@ -77,7 +77,7 @@ class SearchConversationView(AbstractView):
                 datetime.now(), datetime.now(), max_tokens, top_p, temperature),
             Chat(50, 2, "Analyse de données et Visualisation", 
                 datetime.now(), datetime.now(), max_tokens, top_p, temperature)
-            ]   
+            ]   """
              
             from view.userviews.list_conversation_view import ListConversationView
             return ListConversationView(message="Voir l'historique de conversation", conv_list=res, last_view=1)
