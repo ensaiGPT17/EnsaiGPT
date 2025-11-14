f5623707363afc6e3d7376aad8f558d6b85e4187
id2933 <kossi-bruno.amah@eleve.ensai.fr>
2025-11-14 10:08:37 +0000

    les statistiques

--- src/service/chat_service.py
+++ src/service/chat_service.py
@@ -211,18 +211,19 @@ class ChatService:
     def counts_user_message(self, id_user: int):
         chats = self.get_chats_by_id_user(id_user=id_user)
 
-        chats_ids = [c.id_chat for c in chats]
-        message_dao = MessageDAO()
-        nombre_total_de_message = -1
-
         if chats is None:
             return 0
-        
-        for id in chats_ids:
-            messages = message_dao.get_messages_by_chat(id_chat=id)
-            nombre_total_de_message += len(messages)
 
-        return nombre_total_de_messag + 1
+        else:
+            message_dao = MessageDAO()
+            nombre_total_de_message = -1
+
+            chats_ids = [c.id_chat for c in chats]        
+            for id in chats_ids:
+                messages = message_dao.get_messages_by_chat(id_chat=id)
+                nombre_total_de_message += len(messages)
+
+            return nombre_total_de_messag + 1
 
     
     def update_parameters_chat(self, id_chat: int, context: str, max_tokens: int,
