871a18ecd476d5c79fb121b64190f9e39dc44aa4
id2933 <kossi-bruno.amah@eleve.ensai.fr>
2025-11-17 22:35:21 +0000

    test: tous les tests, une bonne fois pour toute

---
+++ tests/conftest.py
@@ -0,0 +1,12 @@
+# tests/conftest.py
+import pytest
+from dao.user_dao import UserDAO
+from dao.chat_dao import ChatDAO
+from dao.message_dao import MessageDAO
+
+@pytest.fixture(autouse=True)
+def reset_singleton():
+    # Réinitialise tous les singletons DAO avant chaque test
+    UserDAO._instances = {}
+    ChatDAO._instances = {}
+    MessageDAO._instances = {}
